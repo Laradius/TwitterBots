a3f5671005612f890ec4dfb79a5742dbb449c32f
Elliot White <elliot.white@petraagency.pl>
2023-02-01 22:25:42 +0100

    irrelevant

--- Program.cs
+++ Program.cs
@@ -1,19 +1,16 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Tweetinvi.Core.Models.Properties;
 using TwitterBot;
 
 static void ConfigureServices(IServiceCollection services)
 {
-    // configure logging
     services.AddLogging(builder =>
     {
         builder.AddConsole();
         builder.AddDebug();
     });
 
-    // build config
     var configuration = new ConfigurationBuilder()
         .SetBasePath(Directory.GetCurrentDirectory())
         .AddJsonFile("appsettings.json", optional: false)
@@ -22,20 +19,13 @@ static void ConfigureServices(IServiceCollection services)
 
     services.Configure<AppSettings>(configuration.GetSection("App"));
 
-    // add services:
-    // services.AddTransient<IMyRespository, MyConcreteRepository>();
-
-    // add app
-    services.AddTransient<MyApp>();
     services.AddTransient<IBot, HorrorBot>();
+    services.AddTransient<MyApp>();
 }
 
-// create service collection
 var services = new ServiceCollection();
 ConfigureServices(services);
 
-// create service provider
 using var serviceProvider = services.BuildServiceProvider();
 
-// entry to run app
 await serviceProvider.GetService<MyApp>().Run(args);
